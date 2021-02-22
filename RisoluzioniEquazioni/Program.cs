adf6c168111ba057476be9ab854798e89f01234e
giuliapupo0@gmail.com <giuliapupo0@gmail.com>
2021-02-22 11:16:57 +0100

    eq

---
+++ RisoluzioniEquazioni/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EquazioniLibrary;
+
+namespace RisoluzioniEquazioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+        }
+    }
+}
